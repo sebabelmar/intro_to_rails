2c8f2b8f68f2ca68ef6104aa306af985d1fa0667
Seba Belmar <seba.belmar@devbootcamp.com>
2016-05-16 23:30:27 +0000

    First Stop: Added view to hello and some controller logic

--- test-1/app/controllers/welcome_controller.rb
+++ test-1/app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
   end
+  
+  def hello
+    a = 4
+    b = 6
+    
+    @number = a + b
+    @title = "This is my favorite number!!"
+  end
 end
